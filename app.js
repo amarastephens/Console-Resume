a63340a054d2427d01231b83963f1311e9164c9c
Amara Stephens <amarastephens@Amaras-MBP.attlocal.net>
2021-09-09 16:06:15 -0500

    final submission

---
+++ app.js
@@ -0,0 +1,27 @@
+let myName = "Amara Stephens";
+console.log(myName.toUpperCase())
+console.log("Career Field: Web Development")
+console.log("Description: Through my experience within the Employee Relations department these past two years and my various Human Resources internships, I have attained many valuable skills. These include soft skills such as having empathy for others, creating positive interpersonal communications, using problem-solving, and exemplifying change agility. I think the assets that have allowed me to be both a successful student in school and an achiever in the workforce is my well-rounded work ethic, my organization skills, my time management, and my ability to easily collaborate with others. With these skills along with the web development skills I have attained in my coding bootcamp, I am hoping to find a job as a junior or mid-level web developer. I want to further the knowledge I have in web design and development and become a positive, eager addition to a web development team.")
+console.log("My Interests: Hiking, Baking, Brewery-Hopping, Watching Documentaries")
+
+function displayPosition(title, company, duties) {
+    console.log(title, company, duties)
+}
+console.log("My Past Work Experience:")
+displayPosition("Human Resources Coordinator", "at Encompass Health", "- I monitored flagged background and drug screen results for the company and assessed eligibility for hire for each candidate")
+displayPosition("Human Resources Intern", "at SMP Automotive Global", "- I assisted in the recruitment of over 1,000 technical and production hourly team members leading up to the start of production")
+displayPosition("Recruiting Intern", "at Iron City Acquisitions", "- I recruited and scouted job seekers for direct sales positions and served as company’s liaison to the public")
+
+console.log("My Skills:")
+let coolSkill = "Javascript"; "Sharepoint"; "Coding"
+function displaySkill(coolSkill) {
+    console.log("BAM:" + coolSkill)
+}
+
+console.log("HTML")
+displaySkill("Javascript")
+console.log("FMLA & ADA")
+console.log("Recruitment")
+displaySkill("Sharepoint")
+displaySkill("Coding")
+
